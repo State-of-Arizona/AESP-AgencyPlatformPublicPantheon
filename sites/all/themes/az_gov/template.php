6c1eeff9ca1d6eaacda352e364633cbbc813d9f8
Thomas Williams <twilliams@vermillion.eaglecrk.local>
2016-04-21 17:26:13 -0500

    adding message to user page

--- sites/all/themes/az_gov/template.php
+++ sites/all/themes/az_gov/template.php
@@ -272,3 +272,17 @@ function az_gov_preprocess_region(&$vars) {
       break;
   }
 }
+
+function az_gov_theme() {
+  $items = array();
+  // create custom user-login.tpl.php
+  $items['user_login'] = array(
+    'render element' => 'form',
+    'path' => drupal_get_path('theme', 'az_gov') . '/templates',
+    'template' => 'user-login',
+    'preprocess functions' => array(
+      'az_gov_preprocess_user_login'
+    ),
+  );
+  return $items;
+}

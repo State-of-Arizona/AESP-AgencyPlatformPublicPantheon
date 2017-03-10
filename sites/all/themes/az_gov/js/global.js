2c591dcb787665b59666fe403bf0d913584c40a5
Thomas Williams <tawiiams@gmail.com>
2017-03-10 12:22:26 -0600

    Mobile menu fix

--- sites/all/themes/az_gov/js/global.js
+++ sites/all/themes/az_gov/js/global.js
@@ -74,30 +74,30 @@
         var $region = $wrapper.closest('.contextual-links-region');
         var $links = $wrapper.find('ul.contextual-links');
         var $trigger = $('<a class="contextual-links-trigger" href="#" />').text(Drupal.t('Configure')).click(
-          function () {
-            $links.stop(true, true).slideToggle(100);
-            $wrapper.toggleClass('contextual-links-active');
-            return false;
-          }
+            function () {
+              $links.stop(true, true).slideToggle(100);
+              $wrapper.toggleClass('contextual-links-active');
+              return false;
+            }
         );
         // Attach hover behavior to trigger and ul.contextual-links.
         $trigger.add($links).hover(
-          function () {
-            $region.addClass('contextual-links-region-active');
-          },
-          function () {
-            $region.removeClass('contextual-links-region-active');
-          }
+            function () {
+              $region.addClass('contextual-links-region-active');
+            },
+            function () {
+              $region.removeClass('contextual-links-region-active');
+            }
         );
         // Hide the contextual links when user clicks a link or rolls out of the .contextual-links-region.
         /*$region.bind('mouseleave click', Drupal.contextualLinks.mouseleave); Did not work for non-admins, modified the bind reaction as below */
         $region.hover(
-          function () {
-            $trigger.addClass('contextual-links-trigger-active');
-          },
-          function () {
-            $trigger.removeClass('contextual-links-trigger-active');
-          }
+            function () {
+              $trigger.addClass('contextual-links-trigger-active');
+            },
+            function () {
+              $trigger.removeClass('contextual-links-trigger-active');
+            }
         );
         // Prepend the trigger.
         $wrapper.prepend($trigger);
@@ -147,9 +147,9 @@
         })
       });
       //Hides mobile menu on load
-      window.onload = function () {
+      $( document ).ready(function () {
         $('.region-menu').hide();
-      };
+      });
 
       //if child menu items are too far to the right in the window, moves them to the left
       var resized = function () {
@@ -171,13 +171,7 @@
             }
           }
         });
-
-        if ($(window).width() > 768) {
-          $('.region-menu').show();
-        } else {
-          $('.region-menu').hide();
-        }
-
+        
         if ($(window).width() >= 768) {
           $('.region-menu ul.menu .glyphicon-minus-sign').each(function () {
             $(this).removeClass('glyphicon-minus-sign');

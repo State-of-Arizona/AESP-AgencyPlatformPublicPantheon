d3f4139178f6b71416db5058fe0c770da1fbe153
Thomas Williams <tawiiams@gmail.com>
2017-03-13 17:18:18 -0500

    Updating ultimate cron to a suported version

--- sites/all/modules/contrib/ultimate_cron/CronRule.class.php
+++ sites/all/modules/contrib/ultimate_cron/CronRule.class.php
@@ -1,13 +1,16 @@
 <?php
 /**
  * @file
- *
- * This class parses cron rules and determines last execution time using least case integer comparison.
+ * This class parses cron rules and determines last
+ * execution time using least case integer comparison.
  */
 
 class CronRule {
 
   public $rule = NULL;
+  public $time = NULL;
+  public $skew = 0;
+
   public $allow_shorthand = FALSE;
   private static $ranges = array(
     'minutes' => array(0, 59),
@@ -17,31 +20,71 @@ class CronRule {
     'weekdays' => array(0, 6),
   );
 
-  private $parsed_rule = array();
-  public $offset = 0;
   private $type = NULL;
+  static private $cache = array();
+  static private $instances = array();
+  private $last_run;
+  private $next_run;
 
   /**
-   * Constructor
+   * Factory method for CronRule instance.
+   *
+   * @param string $rule
+   *   The crontab rule to use.
+   * @param int $time
+   *   The time to test against.
+   * @param int $skew
+   *   Skew for @ flag.
+   *
+   * @return CronRule
+   *   CronRule object.
    */
-  function __construct($rule = NULL) {
-    $this->rule = $rule;
+  static public function factory($rule, $time = NULL, $skew = 0) {
+    if (strpos($rule, '@') === FALSE) {
+      $skew = 0;
+    }
+
+    $time = isset($time) ? (int) $time : time();
+
+    $key = "$rule:$time:$skew";
+    if (isset(self::$instances[$key])) {
+      return self::$instances[$key];
+    }
+    self::$instances[$key] = new CronRule($rule, $time, $skew);
+    return self::$instances[$key];
   }
 
   /**
-   * Expand interval from cronrule part
+   * Constructor.
    *
-   * @param $matches (e.g. 4-43/5+2)
+   * @param string $rule
+   *   The crontab rule to use.
+   * @param int $time
+   *   The time to test against.
+   * @param int $skew
+   *   Skew for @ flag.
+   */
+  public function __construct($rule, $time, $skew) {
+    $this->rule = $rule;
+    $this->time = $time;
+    $this->skew = $skew;
+  }
+
+  /**
+   * Expand interval from cronrule part.
+   *
+   * @param array $matches
+   *   (e.g. 4-43/5+2).
    *   array of matches:
    *     [1] = lower
    *     [2] = upper
    *     [5] = step
    *     [7] = offset
    *
-   * @return
-   *   (string) comma-separated list of values
+   * @return string
+   *   Comma-separated list of values.
    */
-  function expandInterval($matches) {
+  public function expandInterval($matches) {
     $result = array();
 
     $lower = $matches[1];
@@ -49,167 +92,241 @@ class CronRule {
     $step = isset($matches[5]) && $matches[5] != '' ? $matches[5] : 1;
     $offset = isset($matches[7]) && $matches[7] != '' ? $matches[7] : 0;
 
-    if ($step <= 0) return '';
+    if ($step <= 0) {
+      return '';
+    }
+
     $step = ($step > 0) ? $step : 1;
-    for ($i = $lower; $i <= $upper; $i+=$step) {
+    for ($i = $lower; $i <= $upper; $i += $step) {
       $result[] = ($i + $offset) % (self::$ranges[$this->type][1] + 1);
     }
     return implode(',', $result);
   }
 
   /**
-   * Expand range from cronrule part
+   * Prepare part.
    *
-   * @param $rule
-   *   (string) cronrule part, e.g.: 1,2,3,4-43/5
-   * @param $max
-   *   (string) boundaries, e.g.: 0-59
-   * @param $digits
-   *   (int) number of digits of value (leading zeroes)
-   * @return
-   *   (array) array of valid values
+   * @param string $part
+   *   The part.
+   * @param string $type
+   *   Type of part.
+   *
+   * @return string
+   *   The prepared part.
    */
-  function expandRange($rule, $type) {
-    $this->type = $type;
+  public function preparePart($part, $type) {
     $max = implode('-', self::$ranges[$type]);
-    $rule = str_replace("*", $max, $rule);
-    $rule = str_replace("@", $this->offset % (self::$ranges[$type][1] + 1), $rule);
-    $this->parsed_rule[$type] = $rule;
-    $rule = preg_replace_callback('!(\d+)(?:-(\d+))?((/(\d+))?(\+(\d+))?)?!', array($this, 'expandInterval'), $rule);
-    if (!preg_match('/([^0-9\,])/', $rule)) {
-      $rule = explode(',', $rule);
-      rsort($rule);
+    $part = str_replace("*", $max, $part);
+    $part = str_replace("@", $this->skew % (self::$ranges[$type][1] + 1), $part);
+    return $part;
+  }
+
+  /**
+   * Expand range from cronrule part.
+   *
+   * @param string $part
+   *   Cronrule part, e.g.: 1,2,3,4-43/5.
+   * @param string $type
+   *   Type of range (minutes, hours, etc.)
+   *
+   * @return array
+   *   Valid values for this range.
+   */
+  public function expandRange($part, $type) {
+    $this->type = $type;
+    $part = preg_replace_callback('!(\d+)(?:-(\d+))?((/(\d+))?(\+(\d+))?)?!', array($this, 'expandInterval'), $part);
+    if (!preg_match('/([^0-9\,])/', $part)) {
+      $part = explode(',', $part);
+      rsort($part);
     }
     else {
-      $rule = array();
+      $part = array();
     }
-    return $rule;
+    return $part;
   }
 
   /**
    * Pre process rule.
    *
    * @param array &$parts
+   *   Parts of rules to pre process.
    */
-  function preProcessRule(&$parts) {
-    // Allow JAN-DEC
+  public function preProcessRule(&$parts) {
+    // Allow JAN-DEC.
     $months = array(1 => 'jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec');
     $parts[3] = strtr(strtolower($parts[3]), array_flip($months));
 
-    // Allow SUN-SUN
+    // Allow SUN-SUN.
     $days = array('sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat');
     $parts[4] = strtr(strtolower($parts[4]), array_flip($days));
     $parts[4] = str_replace('7', '0', $parts[4]);
+
+    $i = 0;
+    foreach (self::$ranges as $type => $range) {
+      $part =& $parts[$i++];
+      $max = implode('-', $range);
+      $part = str_replace("*", $max, $part);
+      $part = str_replace("@", $this->skew % ($range[1] + 1), $part);
+    }
   }
 
   /**
-   * Post process rule
+   * Post process rule.
    *
-   * @param array $intervals
+   * @param array &$intervals
+   *   Intervals to post process.
    */
-  function postProcessRule(&$intervals) {
+  public function postProcessRule(&$intervals) {
   }
 
   /**
-   * Generate regex rules
+   * Generate regex rules.
    *
-   * @param $rule
-   *   (string) cronrule, e.g: 1,2,3,4-43/5 * * * 2,5
-   * @return
-   *   (array) date and time regular expression for mathing rule
+   * @return array
+   *   Date and time regular expression for mathing rule.
    */
-  function getIntervals($rule = NULL) {
-    $parts = preg_split('/\s+/', isset($rule) ? $rule : $this->rule);
-    if ($this->allow_shorthand) $parts += array('*', '*', '*', '*', '*'); // Allow short rules by appending wildcards?
-    if (count($parts) != 5) return FALSE;
+  public function getIntervals() {
+    if (isset(self::$cache['intervals'][$this->rule][$this->skew])) {
+      return self::$cache['intervals'][$this->rule][$this->skew];
+    }
+
+    $parts = preg_split('/\s+/', $this->rule);
+    if ($this->allow_shorthand) {
+      // Allow short rules by appending wildcards?
+      $parts += array('*', '*', '*', '*', '*');
+      $parts = array_slice($parts, 0, 5);
+    }
+    if (count($parts) != 5) {
+      return self::$cache['intervals'][$this->rule][$this->skew] = FALSE;
+    }
     $this->preProcessRule($parts);
     $intervals = array();
+    $intervals['parts'] = $parts;
     $intervals['minutes']  = $this->expandRange($parts[0], 'minutes');
-    if (empty($intervals['minutes'])) return FALSE;
+    if (empty($intervals['minutes'])) {
+      return self::$cache['intervals'][$this->rule][$this->skew] = FALSE;
+    }
     $intervals['hours']    = $this->expandRange($parts[1], 'hours');
-    if (empty($intervals['hours'])) return FALSE;
+    if (empty($intervals['hours'])) {
+      return self::$cache['intervals'][$this->rule][$this->skew] = FALSE;
+    }
     $intervals['days']     = $this->expandRange($parts[2], 'days');
-    if (empty($intervals['days'])) return FALSE;
+    if (empty($intervals['days'])) {
+      return self::$cache['intervals'][$this->rule][$this->skew] = FALSE;
+    }
     $intervals['months']   = $this->expandRange($parts[3], 'months');
-    if (empty($intervals['months'])) return FALSE;
+    if (empty($intervals['months'])) {
+      return self::$cache['intervals'][$this->rule][$this->skew] = FALSE;
+    }
     $intervals['weekdays'] = $this->expandRange($parts[4], 'weekdays');
-    if (empty($intervals['weekdays'])) return FALSE;
+    if (empty($intervals['weekdays'])) {
+      return self::$cache['intervals'][$this->rule][$this->skew] = FALSE;
+    }
     $intervals['weekdays'] = array_flip($intervals['weekdays']);
     $this->postProcessRule($intervals);
 
-    return $intervals;
+    return self::$cache['intervals'][$this->rule][$this->skew] = $intervals;
   }
 
   /**
-   * Convert intervals back into crontab rule format
-   */
-  function rebuildRule($intervals) {
-    $parts = array();
-    foreach ($intervals as $type => $interval) {
-      $parts[] = $this->parsed_rule[$type];
-    }
-    return implode(' ', $parts);
-  }
-
-  /**
-   * Parse rule. Run through parser expanding expression, and recombine into crontab syntax.
-   */
-  function parseRule() {
-    return $this->rebuildRule($this->getIntervals());
-  }
-
-  /**
-   * Get last execution time of rule in unix timestamp format
+   * Convert intervals back into crontab rule format.
    *
-   * @param $time
-   *   (int) time to use as relative time (default now)
-   * @return
-   *   (int) unix timestamp of last execution time
+   * @param array $intervals
+   *   Intervals to convert.
+   *
+   * @return string
+   *   Crontab rule.
    */
-  function getLastRan($time = NULL) {
-    // Current time round to last minute
-    if (!isset($time)) $time = time();
-    $time = floor($time / 60) * 60;
+  public function rebuildRule($intervals) {
+    return implode(' ', $intervals['parts']);
+  }
 
-    // Generate regular expressions from rule
+  /**
+   * Parse rule.
+   *
+   * Run through parser expanding expression, and recombine into crontab syntax.
+   */
+  public function parseRule() {
+    if (isset($this->parsed)) {
+      return $this->parsed;
+    }
+    $this->parsed = $this->rebuildRule($this->getIntervals());
+    return $this->parsed;
+  }
+
+  /**
+   * Get last schedule time of rule in UNIX timestamp format.
+   *
+   * @return int
+   *   UNIX timestamp of last schedule time.
+   */
+  public function getLastSchedule() {
+    if (isset($this->last_run)) {
+      return $this->last_run;
+    }
+
+    // Current time round to last minute.
+    $time = floor($this->time / 60) * 60;
+
+    // Generate regular expressions from rule.
     $intervals = $this->getIntervals();
-    if ($intervals === FALSE) return FALSE;
+    if ($intervals === FALSE) {
+      return FALSE;
+    }
 
-    // Get starting points
+    // Get starting points.
     $start_year   = date('Y', $time);
-    $end_year     = $start_year - 28; // Go back max 28 years (leapyear * weekdays)
+    // Go back max 28 years (leapyear * weekdays).
+    $end_year     = $start_year - 28;
     $start_month  = date('n', $time);
     $start_day    = date('j', $time);
     $start_hour   = date('G', $time);
-    $start_minute = (int)date('i', $time);
+    $start_minute = (int) date('i', $time);
 
     // If both weekday and days are restricted, then use either or
-    // otherwise, use and ... when using or, we have to try out all the days in the month
-    // and not just to the ones restricted
-    $check_both = (count($intervals['days']) != 31 && count($intervals['weekdays']) != 7) ? FALSE : TRUE;
-    $days = $check_both ? $intervals['days'] : range(31, 1);
+    // otherwise, use and ... when using or, we have to try out all the days
+    // in the month and not just to the ones restricted.
+    $check_weekday = count($intervals['weekdays']) != 7;
+    $check_both = $check_weekday && (count($intervals['days']) != 31);
+    $days = $check_both ? range(31, 1) : $intervals['days'];
 
-    // Find last date and time this rule was run
+    // Find last date and time this rule was run.
     for ($year = $start_year; $year > $end_year; $year--) {
       foreach ($intervals['months'] as $month) {
-        if ($month < 1 || $month > 12) continue;
-        if ($year >= $start_year && $month > $start_month) continue;
+        if ($month < 1 || $month > 12) {
+          continue;
+        }
+        if ($year >= $start_year && $month > $start_month) {
+          continue;
+        }
 
         foreach ($days as $day) {
-          if ($day < 1 || $day > 31) continue;
-          if ($year >= $start_year && $month >= $start_month && $day > $start_day) continue;
-          if (!checkdate($month, $day, $year)) continue;
-
-          // Check days and weekdays using and/or logic
-          $date_array = getdate(mktime(0, 0, 0, $month, $day, $year));
-          if ($check_both) {
-            if (!isset($intervals['weekdays'][$date_array['wday']])) continue;
+          if ($day < 1 || $day > 31) {
+            continue;
           }
-          else {
-            if (
-              !in_array($day, $intervals['days']) &&
-              !isset($intervals['weekdays'][$date_array['wday']])
-            ) continue;
+          if ($year >= $start_year && $month >= $start_month && $day > $start_day) {
+            continue;
+          }
+          if (!checkdate($month, $day, $year)) {
+            continue;
+          }
+
+          // Check days and weekdays using and/or logic.
+          if ($check_weekday) {
+            $date_array = getdate(mktime(0, 0, 0, $month, $day, $year));
+            if ($check_both) {
+              if (
+                !in_array($day, $intervals['days']) &&
+                !isset($intervals['weekdays'][$date_array['wday']])
+              ) {
+                continue;
+              }
+            }
+            else {
+              if (!isset($intervals['weekdays'][$date_array['wday']])) {
+                continue;
+              }
+            }
           }
 
           if ($day != $start_day || $month != $start_month || $year != $start_year) {
@@ -217,12 +334,22 @@ class CronRule {
             $start_minute = 59;
           }
           foreach ($intervals['hours'] as $hour) {
-            if ($hour < 0 || $hour > 23) continue;
-            if ($hour > $start_hour) continue;
-            if ($hour < $start_hour) $start_minute = 59;
+            if ($hour < 0 || $hour > 23) {
+              continue;
+            }
+            if ($hour > $start_hour) {
+              continue;
+            }
+            if ($hour < $start_hour) {
+              $start_minute = 59;
+            }
             foreach ($intervals['minutes'] as $minute) {
-              if ($minute < 0 || $minute > 59) continue;
-              if ($minute > $start_minute) continue;
+              if ($minute < 0 || $minute > 59) {
+                continue;
+              }
+              if ($minute > $start_minute) {
+                continue;
+              }
               break 5;
             }
           }
@@ -230,16 +357,56 @@ class CronRule {
       }
     }
 
-    // Create unix timestamp from derived date+time
-    $time = mktime($hour, $minute, 0, $month, $day, $year);
+    if (!isset($hour) || !isset($minute) || !isset($month) || !isset($day) || !isset($year)) {
+      return FALSE;
+    }
 
-    return $time;
+    // Create UNIX timestamp from derived date+time.
+    $this->last_run = mktime($hour, $minute, 0, $month, $day, $year);
+
+    return $this->last_run;
   }
 
   /**
-   * Check if a rule is valid
+   * Get next schedule time of rule in UNIX timestamp format.
+   *
+   * @return int
+   *   UNIX timestamp of next schedule time.
    */
-  function isValid($time = NULL) {
-    return $this->getLastRan($time) === FALSE ? FALSE : TRUE;
+  public function getNextSchedule() {
+    $time = $this->time;
+    $last_schedule = $this->getLastSchedule();
+    $next_schedule = NULL;
+
+    // Do a binary search for the next schedule.
+    $interval = 86400 * 30;
+    $offset = $interval;
+    do {
+      $test = new CronRule($this->rule, $time + (int) $offset, $this->skew);
+      $schedule = $test->getLastSchedule();
+
+      $interval /= 2;
+      if ($schedule > $last_schedule) {
+        $next_schedule = $schedule;
+        $offset -= $interval;
+      }
+      elseif ($next_schedule) {
+        $offset += $interval;
+      }
+      else {
+        // Increase interval by doubling up.
+        // (we've already halved it, so now we quadrouple it).
+        $offset = $interval *= 4;
+      }
+    } while ($interval > 30);
+
+    return $next_schedule;
+  }
+
+  /**
+   * Check if a rule is valid.
+   */
+  public function isValid() {
+    return $this->getLastSchedule() === FALSE ? FALSE : TRUE;
   }
 }

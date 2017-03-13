a4fa6584bf0d047f8c0812f5b43978fc51208c51
Thomas Williams <tawiiams@gmail.com>
2017-03-13 17:08:09 -0500

    Updating to suported version of views fieldsets

--- sites/all/modules/contrib/views_fieldsets/templates/views-fieldsets-fieldset.tpl.php
+++ sites/all/modules/contrib/views_fieldsets/templates/views-fieldsets-fieldset.tpl.php
@@ -1,19 +1,14 @@
 
-<<?php print $tag; ?> class="<?php print $classes; ?>"<?php print $attributes; ?>>
+<fieldset class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
-  <?php if ($legend_tag): ?>
-    <<?php print $legend_tag; ?>>
-      <span class="fieldset-legend"><?php print $legend; ?></span>
-    </<?php print $legend_tag; ?>>
-    <div class="fieldset-wrapper">
-  <?php endif; ?>
+  <legend>
+    <span class="fieldset-legend"><?php print $legend; ?></span>
+  </legend>
 
-  <?php foreach ($fieldset_fields as $name => $field): ?>
-    <?php print @$field->separator . $field->wrapper_prefix . $field->label_html . $field->content . $field->wrapper_suffix; ?>
-  <?php endforeach; ?>
+  <div class="fieldset-wrapper">
+    <?php foreach ($fieldset_fields as $name => $field): ?>
+      <?php print @$field->separator . $field->wrapper_prefix . $field->label_html . $field->content . $field->wrapper_suffix; ?>
+    <?php endforeach; ?>
+  </div>
 
-  <?php if ($legend_tag): ?>
-    </div><?php /* .fieldset-wrapper */ ?>
-  <?php endif; ?>
-
-</<?php print $tag; ?>>
+</fieldset>

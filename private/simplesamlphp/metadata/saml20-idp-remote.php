20a67c944318b12d01e22d460905d2c59671b86a
Thomas Williams <twilliams@eaglecrk.com>
2018-12-18 18:35:50 -0600

    adding simple saml

---
+++ private/simplesamlphp/metadata/saml20-idp-remote.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * SAML 2.0 remote IdP metadata for SimpleSAMLphp.
+ *
+ * Remember to remove the IdPs you don't use from this file.
+ *
+ * See: https://simplesamlphp.org/docs/stable/simplesamlphp-reference-idp-remote 
+ */
+
+//====================================== Metadata for live ===================================================
+//============================================================================================================
+
+if (isset($_SERVER['PANTHEON_ENVIRONMENT']) && $_SERVER['PANTHEON_ENVIRONMENT'] === 'live' ) {
+
+	//Enter converted metadata for live here
+
+}
+
+
+//====================================== Metadata for test ===================================================
+//============================================================================================================
+
+if (isset($_SERVER['PANTHEON_ENVIRONMENT']) && $_SERVER['PANTHEON_ENVIRONMENT'] === 'test' ) {
+
+	//Enter converted metadata for test here
+
+} 
+
+
+//====================================== Metadata for dev  ===================================================
+//============================================================================================================
+
+if (isset($_SERVER['PANTHEON_ENVIRONMENT']) && $_SERVER['PANTHEON_ENVIRONMENT'] === 'dev' ) {
+
+	//Enter converted metadata for dev here
+
+}
+
+

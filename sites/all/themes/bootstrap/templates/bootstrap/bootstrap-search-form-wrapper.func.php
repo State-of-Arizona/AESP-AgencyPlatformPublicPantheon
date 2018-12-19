a427ce48eee3edb4c363beac7ab0872d14677d1b
Thomas Williams <twilliams@eaglecrk.com>
2018-12-18 18:29:36 -0600

    bootstrap theme update to 7.x-3.22

--- sites/all/themes/bootstrap/templates/bootstrap/bootstrap-search-form-wrapper.func.php
+++ sites/all/themes/bootstrap/templates/bootstrap/bootstrap-search-form-wrapper.func.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @file
  * Stub file for bootstrap_bootstrap_search_form_wrapper().
@@ -7,13 +8,19 @@
 /**
  * Returns HTML for the Bootstrap search form wrapper.
  *
+ * @param array $variables
+ *   An associative array of variables.
+ *
+ * @return string
+ *   The constructed HTML markup.
+ *
  * @ingroup theme_functions
  */
-function bootstrap_bootstrap_search_form_wrapper($variables) {
+function bootstrap_bootstrap_search_form_wrapper(array $variables) {
   $output = '<div class="input-group">';
   $output .= $variables['element']['#children'];
   $output .= '<span class="input-group-btn">';
-  $output .= '<button type="submit" class="btn btn-primary" aria-label="Search">' . _bootstrap_icon('search', t('Search')) . '</button>';
+  $output .= '<button type="submit" class="btn btn-primary">' . _bootstrap_icon('search', t('Search')) . '</button>';
   $output .= '</span>';
   $output .= '</div>';
   return $output;

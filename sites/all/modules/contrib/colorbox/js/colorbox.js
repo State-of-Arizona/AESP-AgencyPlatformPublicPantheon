bc1dd1fe8d281cacf6c6dd3bdb966743d8376504
Thomas Williams <twilliams@atos.net>
2022-02-16 17:05:20 -0600

    Colorbox update

--- sites/all/modules/contrib/colorbox/js/colorbox.js
+++ sites/all/modules/contrib/colorbox/js/colorbox.js
@@ -1,8 +1,13 @@
+/**
+ * @file
+ * Colorbox module init js.
+ */
+
 (function ($) {
 
 Drupal.behaviors.initColorbox = {
   attach: function (context, settings) {
-    if (!$.isFunction($.colorbox) || typeof settings.colorbox === 'undefined') {
+    if (!$.isFunction($('a, area, input', context).colorbox) || typeof settings.colorbox === 'undefined') {
       return;
     }
 
@@ -14,9 +19,30 @@ Drupal.behaviors.initColorbox = {
       }
     }
 
+    // Use "data-colorbox-gallery" if set otherwise use "rel".
+    settings.colorbox.rel = function () {
+      if ($(this).data('colorbox-gallery')) {
+        return $(this).data('colorbox-gallery');
+      }
+      else {
+        return $(this).attr('rel');
+      }
+    };
+
     $('.colorbox', context)
-      .once('init-colorbox')
-      .colorbox(settings.colorbox);
+      .once('init-colorbox').each(function(){
+        // Only images are supported for the "colorbox" class.
+        // The "photo" setting forces the href attribute to be treated as an image.
+        var extendParams = {
+          photo: true
+        };
+        // If a title attribute is supplied, sanitize it.
+        var title = $(this).attr('title');
+        if (title) {
+          extendParams.title = Drupal.checkPlain(title);
+        }
+        $(this).colorbox($.extend({}, settings.colorbox, extendParams));
+      });
 
     $(context).bind('cbox_complete', function () {
       Drupal.attachBehaviors('#cboxLoadedContent');

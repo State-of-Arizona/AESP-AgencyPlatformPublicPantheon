95346c2d1f668a987230609949ce647031782cab
Elle Paynter <danielle.paynter@azdoa.gov>
2024-10-17 10:30:46 -0700

    AZ Sliver | Update http/https

--- sites/all/modules/custom/az_site_sliver/template/sliver.tpl.php
+++ sites/all/modules/custom/az_site_sliver/template/sliver.tpl.php
@@ -10,7 +10,7 @@
       <ul class="sliver-nav">
         <li class="sliver-item state-logo"><a href="https://az.gov" target="_blank"><span class="sr-only">State of Arizona</span></a></li>
         <li class="sliver-item state-services openbooks"><a href="https://openbooks.az.gov/" target="_blank"><span class="main-text"><span class="mobilehide">Visit </span>OpenBooks</span></a></li>
-        <li class="sliver-item state-services ombudsman"><a href="http://www.azoca.gov/" target="_blank"><span class="main-text"><span class="mobilehide">Ombudsman </span>Citizens Aide</span></a></li>
+        <li class="sliver-item state-services ombudsman"><a href="https://www.azoca.gov/" target="_blank"><span class="main-text"><span class="mobilehide">Ombudsman </span>Citizens Aide</span></a></li>
         <li class="sliver-item state-services vote"><a href="https://azsos.gov/elections/voters/register-vote-update-voter-information/registerupdate-vote/" target="_blank"><span class="main-text"><span class="mobilehide">Register to </span>Vote</span></a></li>
       </ul>
       <section class="search-bar">
